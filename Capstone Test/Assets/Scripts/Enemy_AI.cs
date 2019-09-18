22dc7073f163ed4afdb14ae1763f1d4f2e918d91
zacharytay1994 <s10179991@connect.np.edu.sg>
2019-09-19 05:20:57 +0800

    added a* pathfinding, added fsm, added enemy components

---
+++ Capstone Test/Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Enemy_AI : MonoBehaviour
+{
+    public GameObject target_;
+
+    public float speed_ = 200.0f;
+    public float next_waypoint_distance_ = 1.0f;
+    Quaternion rotation_;
+
+    // Pathfinding variables
+    Path path_;
+    int current_waypoint_ = 0;
+    bool reached_end_path_ = false;
+
+    Seeker seeker_;
+    Rigidbody2D rb2D_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker_ = GetComponent<Seeker>();
+        rb2D_ = GetComponent<Rigidbody2D>();
+        target_ = GameObject.Find("Player 1");
+
+        InvokeRepeating("UpdatePath", 0.0f, 1.0f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker_.IsDone())
+        {
+            seeker_.StartPath(rb2D_.position, target_.transform.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path path)
+    {
+        if (!path.error)
+        {
+            path_ = path;
+            current_waypoint_ = 0;
+            print("completed");
+        } 
+    }
+
+    //void LookAt(Vector3 target)
+    //{
+    //    rotation_ = Quaternion.LookRotation(target - transform.position, Vector3.up);
+    //    transform.rotation = rotation_;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path_ == null)
+        {
+            return;
+        }
+        // if reached end of path
+        if (current_waypoint_ >= path_.vectorPath.Count)
+        {
+            reached_end_path_ = true;
+            return;
+        }
+        else
+        {
+            reached_end_path_ = false;
+        }
+
+        Vector2 direction_ = ((Vector2)path_.vectorPath[current_waypoint_] - rb2D_.position).normalized;
+        Vector2 force_ = direction_ * speed_ * Time.deltaTime;
+        rb2D_.AddForce(force_);
+
+        //LookAt(path_.vectorPath[current_waypoint_]);
+
+        float distance_ = Vector2.Distance(rb2D_.position, path_.vectorPath[current_waypoint_]);
+
+        if (distance_ < next_waypoint_distance_)
+        {
+            current_waypoint_++;
+        }
+    }
+}
--- Capstone Test/Assets/Scripts/Enemy_Main.cs
+++ Capstone Test/Assets/Scripts/Enemy_Main.cs
@@ -2,64 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface State
+public abstract class State
 {
-    void Enter();
-    void Execute();
-    void Exit();
+    public abstract void Enter(ref Enemy_Main gameobject);
+    public abstract void Execute(ref Enemy_Main gameobject);
+    public abstract void Exit(ref Enemy_Main gameobject);
 }
 
 public class StateMachine
 {
-    private State current_state_;
-    private State previous_state_;
-    private State global_state_;
+    public State current_state_;
+    public State previous_state_;
+    public State global_state_;
+    private Enemy_Main owner_;
 
-    void ChangeState(State newstate)
+    public StateMachine(Enemy_Main owner)
+    {
+        owner_ = owner;
+        current_state_ = new Idle();
+    }
+
+    public void ChangeState(State newstate)
     {
         if (current_state_ != null)
         {
-            current_state_.Exit();
+            current_state_.Exit(ref owner_);
+            Debug.Log("reached");
             previous_state_ = current_state_;
             current_state_ = newstate;
-            current_state_.Enter();
+            current_state_.Enter(ref owner_);
         }
     }
 
-    void Update()
+    public void Update()
     {
         if (current_state_ != null)
         {
-            current_state_.Execute();
+            current_state_.Execute(ref owner_);
         }
         if (global_state_ != null)
         {
-            global_state_.Execute();
+            global_state_.Execute(ref owner_);
         }
     }
 
-    void RevertState()
+    public void RevertState()
     {
         ChangeState(previous_state_); 
     }
 
-    bool IsInState(State state)
+    public bool IsInState(State state)
     {
         return state == current_state_;
+    }
+
+    public void InitState(State state)
+    {
+        current_state_ = state;
+    }
+}
+
+public class Idle : State
+{
+    public override void Enter(ref Enemy_Main gameobject)
+    {
+        Debug.Log("idle state entered");
+    }
+    public override void Execute(ref Enemy_Main gameobject)
+    {
+        GameObject game_object = GameObject.Find("Player 1");
+        if (game_object != null)
+        {
+            gameobject.target_ = game_object;
+            gameobject.state_machine_.ChangeState(new Chase());
+        }
+    }
+    public override void Exit(ref Enemy_Main gameobject)
+    {
+    }
+}
+
+public class Chase : State
+{
+    public override void Enter(ref Enemy_Main gameobject)
+    {
+        if (gameobject.target_ == null)
+        {
+            gameobject.state_machine_.ChangeState(new Idle());
+        }
+    }
+    public override void Execute(ref Enemy_Main gameobject)
+    {
+        if (gameobject.target_ != null)
+        {
+            
+        }
+    }
+    public override void Exit(ref Enemy_Main gameobject)
+    {
     }
 }
 
 public class Enemy_Main : MonoBehaviour
 {
+    public StateMachine state_machine_;
+    public GameObject target_;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        state_machine_ = new StateMachine(this);
+        //state_machine_.InitState(new Idle());
+        Debug.Log(state_machine_.current_state_);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        state_machine_.Update();
     }
 }
