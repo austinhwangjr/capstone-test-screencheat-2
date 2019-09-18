6ffef76ab7b735e1e0e4dbfffcd95e12fef9ca1a
zacharytay1994 <s10179991@connect.np.edu.sg>
2019-09-18 23:43:20 +0800

    added enemy, added enemy script, added instance manager

---
+++ Capstone Test/Assets/Scripts/Enemy_Main.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface State
+{
+    void Enter();
+    void Execute();
+    void Exit();
+}
+
+public class StateMachine
+{
+    private State current_state_;
+    private State previous_state_;
+    private State global_state_;
+
+    void ChangeState(State newstate)
+    {
+        if (current_state_ != null)
+        {
+            current_state_.Exit();
+            previous_state_ = current_state_;
+            current_state_ = newstate;
+            current_state_.Enter();
+        }
+    }
+
+    void Update()
+    {
+        if (current_state_ != null)
+        {
+            current_state_.Execute();
+        }
+        if (global_state_ != null)
+        {
+            global_state_.Execute();
+        }
+    }
+
+    void RevertState()
+    {
+        ChangeState(previous_state_); 
+    }
+
+    bool IsInState(State state)
+    {
+        return state == current_state_;
+    }
+}
+
+public class Enemy_Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Capstone Test/Assets/Scripts/Instance_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Instance_Manager : MonoBehaviour
+{
+    private GameObject[] player_list_;
+    private GameObject[] obstacle_list_;
+    private List<GameObject> enemy_list_ = new List<GameObject>();
+
+    // Prefab references
+    private GameObject Enemy_Prefab;
+
+    // Positions
+    private Vector3 map_center_ = new Vector3(0.0f, 0.0f, 4.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_list_ = GameObject.FindGameObjectsWithTag("Player");
+        obstacle_list_ = GameObject.FindGameObjectsWithTag("Obstacle");
+
+        // Instantiating prefabs
+        Enemy_Prefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Enemy.prefab", typeof(GameObject));
+        AddEnemy(map_center_);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(enemy_list_.Count);
+    }
+
+    void AddEnemy(Vector3 position)
+    {
+        enemy_list_.Add(Instantiate(Enemy_Prefab, map_center_, Quaternion.identity));
+    }
+}
